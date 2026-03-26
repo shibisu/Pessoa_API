bd062d90c487f096537cee029e9b5fa40f673b9a
IGOR EUGENIO GOMES PACHECO PEREIRA <igor.pereira3@alunos>
2026-03-26 19:47:02 -0300

    Primeira Pessoa

--- src/main/java/br/com/senac/pessoa_api/Controller/PossoasController.java
+++ src/main/java/br/com/senac/pessoa_api/Controller/PossoasController.java
@@ -1,11 +1,13 @@
 package br.com.senac.pessoa_api.Controller;
 
+import br.com.senac.pessoa_api.dtos.PessoasResquestDTO;
 import br.com.senac.pessoa_api.entidades.Pessoas;
 import br.com.senac.pessoa_api.repositores.PessoasRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,5 +30,13 @@ public class PossoasController {
             return ResponseEntity.status(204).body(null);
         }
         return ResponseEntity.ok(pessoasList);
+    }
+    public  ResponseEntity<Pessoas> criar(@RequestBody PessoasResquestDTO pessoa){
+
+        Pessoas pessoaPesist = new Pessoas();
+        pessoaPesist.setNome(pessoa.getNome());
+        pessoaPesist.setSobenome(pessoa.getSobrenome());
+        pessoasRepository.save(pessoaPesist);
+        return ResponseEntity.status(201).body()
     }
 }
